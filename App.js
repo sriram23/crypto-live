74eb8aebe31a0c425024e37e7acb8e213ac57802
Sriram <sriramsubramanian23@gmail.com>
2024-10-02 21:33:08 +0530

    Implemented websocket

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Socket from './components/socket';
 
 const App = () => {
     return(
         <div>
-            <h1>Hello World!</h1>
+            <Socket/>
         </div>
     )
 }
