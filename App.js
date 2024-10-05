8e8b4ea9e9e4020cc1f2c0fa1437eb8e8e813faf
Sriram <sriramsubramanian23@gmail.com>
2024-10-05 11:47:21 +0530

    Added drawer

--- App.js
+++ App.js
@@ -1,17 +1,72 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Box, Text, ChakraProvider } from '@chakra-ui/react';
-import Home from './page/home';
+import React, {useState} from "react";
+import ReactDOM from "react-dom/client";
+import {
+  Box,
+  Text,
+  ChakraProvider,
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  Button,
+  Icon
+} from "@chakra-ui/react";
+import { AiOutlineMenu } from "react-icons/ai";
+import Home from "./page/home";
 
 const App = () => {
-    return(
-        <ChakraProvider>
-            <Box border="1px solid #202020" p={6} position="sticky" top={0} zIndex={999} bgGradient="radial-gradient(circle at center, #121212, #0d0d0d)">
-                <Text fontSize="3xl" textAlign="center" as="h1">🪙 Crypto Currency Live 🔴</Text>
-            </Box>
-            <Home/>
-        </ChakraProvider>
-    )
-}
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App/>)
+    const [isOpen, setIsOpen] = useState(false)
+    const onClose = () => {
+        setIsOpen(false);
+    }
+  return (
+    <ChakraProvider>
+      <Box
+        border="1px solid #202020"
+        p={6}
+        position="sticky"
+        top={0}
+        zIndex={999}
+        bgGradient="radial-gradient(circle at center, #121212, #0d0d0d)"
+      >
+        <Text fontSize="3xl" textAlign="center" as="h1">
+          🪙 Crypto Currency Live 🔴
+        </Text>
+        {/* <IconButton icon={<HamburgerIcon/>} /> */}
+        <Button bg="#202020" color="white" position="absolute" right={10} top={5} onClick={() => setIsOpen(true)}><Icon as={AiOutlineMenu}/></Button>
+      </Box>
+      <Home />
+
+      {/* Drawer */}
+      <Drawer
+        isOpen={isOpen}
+        placement='right'
+        onClose={onClose}
+        // finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent bgGradient="radial-gradient(circle at center, #121212, #0d0d0d)">
+          <DrawerCloseButton />
+          <DrawerHeader>Create your account</DrawerHeader>
+
+          <DrawerBody>
+            {/* <Input placeholder='Type here...' /> */}
+            Hello World!
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Button variant='outline' mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme='blue'>Save</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </ChakraProvider>
+  );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
